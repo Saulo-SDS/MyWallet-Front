1a76145a114fea31ad3ef8611019b5eb72f53fc9
Saulo <saulosilva@super.ufam.edu.br>
2021-10-23 20:39:07 -0400

    Added payment exit post

--- src/Pages/NewExit/index.js
+++ src/Pages/NewExit/index.js
@@ -1,16 +1,78 @@
-import Top from "../../Components/Top";
 import styled from "styled-components";
 import { Button, Form, Input } from "../../Components/Shared";
+import Top from "../../Components/Top";
+import { useState, useContext } from "react";
+import Loader from 'react-loader-spinner';
+import Swal from "sweetalert2";
+import { useHistory } from "react-router-dom";
+import { UserContext } from "../../contexts/UserContext";
+import { postPayment } from "../../Components/Service/Api";
+import dayjs from "dayjs";
 
 function NewExit() {
+
+    let history = useHistory();
+    const [value, setValue] = useState("");
+    const [describe, setDescribe] = useState("");
+    const [loading, setLoading] = useState(false);
+    const { user, setUser } = useContext(UserContext);
+
+    function saveExit(e) {
+
+        e.preventDefault();
+        setLoading(true);
+
+        const config = {
+            headers: {
+                "Authorization": `Bearer ${user.token}`
+            }
+        };
+        
+        const date = dayjs().format("YYYY-MM-DD");
+        const type = "exit";
+        const body = {
+            value, 
+            type, 
+            describe, 
+            date
+        };
+
+        postPayment(body, config)
+        .then(res => {
+            history.push("/user/payments");
+            setLoading(false);
+        })
+        .catch(err => {
+            Swal.fire({
+                icon:'error',
+                title:'Oops...',
+                text:'Dados inválidos tente novamente'
+            });
+            setLoading(false);
+        });
+    }
 
     return (
         <Container>
             <Top text="Nova saida"/>
-            <Form>
-                <Input placeholder="Valor"/>
-                <Input placeholder="Descrição"/>
-                <Button>Salvar saida</Button>
+            <Form onSubmit={saveExit}>
+               <Input 
+                    type="number"
+                    step="0.01" 
+                    placeholder="Valor"
+                    onChange={(e) => setValue(e.target.value)} 
+                    required
+                />
+
+                <Input 
+                    type="text" 
+                    placeholder="Descrição" 
+                    onChange={(e) => setDescribe(e.target.value)}
+                    required
+                />
+                <Button type="submit" disabled={loading}>
+                    {!loading ? "Salvar saida" :  <Loader type="ThreeDots" color="#FFFFFF" height={13} width={80} />}
+                </Button>
             </Form>
         </Container>
     );
